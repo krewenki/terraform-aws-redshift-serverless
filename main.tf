7b2f520b7c0b6de294de826dbdad881bdf15d4a4
daurelio <diogo@mklabs.io>
2023-10-19 23:02:53 +0200

    refactor(terraform): rm unsed locals
    Signed-off-by: daurelio <diogo@mklabs.io>

--- main.tf
+++ main.tf
@@ -15,8 +15,6 @@ resource "random_password" "master_password" {
 ################################################################################
 
 locals {
-  subnet_group_name    = var.create && var.create_subnet_group ? aws_redshift_subnet_group.this[0].name : var.subnet_group_name
-  parameter_group_name = var.create && var.create_parameter_group ? aws_redshift_parameter_group.this[0].id : var.parameter_group_name
 
   master_password = var.create && var.create_random_password ? random_password.master_password[0].result : var.master_password
 }
